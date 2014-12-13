95e668dca54703b7108733352112c75303bfab74
School(Re)mix <guillaume.rouan@ac-rennes.fr>
2014-12-13 21:14:53 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?PHP	
+	/* --- LOCOCHOCO : LA PIEUVRE DES MATHS ---
+	 
+	   Licence	: CC BY International 4.0
+	   Auteur	: School(Re)mix
+	   Date	: 2014
+	
+	---------------------------------------- */
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>locochoco</title>
+    
+    <link rel="stylesheet" media="screen" type="text/css" title="locochoco" href="css/locochoco.css" />
+
+</head>
+
+<body>
+	<a href="classe_de_ce1.php" target="_self" title="locochoco : la pieuvre des maths">
+       <div id="page">
+              <div id="ariane">locochoco : la pieuvre des maths</div>
+              <div id="classe">
+                     <h1 class="bloc">calcul mental</h1>
+               </div>
+               <!-- <a href="#" target="_self" title="classe de CE2"><div id="classe" class="CE2"><h1 class="bloc">CE2</h1></div></a>
+               <a href="#" target="_self" title="classe de CM1"><div id="classe" class="CM1"><h1 class="bloc">CM1</h1></div></a>
+               <a href="#" target="_self" title="classe de CM2"><div id="classe" class="CM2"><h1 class="bloc">CM2</h1></div></a> -->
+    	</div>
+       </a>
+</body>
+</html>
